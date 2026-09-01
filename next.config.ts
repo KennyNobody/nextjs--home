ccb879f44bb3cce0ee83126cca28acacc26a29b5
Egor Badulin <nobodykenny@gmail.com>
2026-09-02 01:51:34 +0300

    feat: edit images config

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,7 @@ const nextConfig: NextConfig = {
     //     ]
     // },
     images: {
+        dangerouslyAllowLocalIP: true,
         remotePatterns: [
             {
                 protocol: 'https',
